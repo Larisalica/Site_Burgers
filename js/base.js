ee47b5fd2f23c7a2bace148c20ae33d6b205f39c
Larisa Kanaeva <alice.trueorfalse@yandex.ru>
2018-02-11 20:20:14 +0400

    dont work

--- js/base.js
+++ js/base.js
@@ -4,16 +4,24 @@ hamburger.addEventListener ("click", function(e){
 
 e.preventDefault();
 
-var nav = document.getElementById("nav-list");
+var nav = document.getElementById("nav");
   console.log(nav);
+nav.classList.add("nav-active");
 
-nav.classList.add("nav-list_active");
-
-
+var hamburger = document.getElementById("hamburger");
+console.log(hamburger);
+hamburger.classList.add("hamburger-active");
 });
 
 
 var menu__accordeon = document.getElementById("menu__accordeon-item");
 console.log (menu__accordeon);
 
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:1
+  });
+});
   
