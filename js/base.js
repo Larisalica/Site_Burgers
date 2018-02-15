2311ad9fbe8d2f17c09403fff542abf66b053388
Larisa Kanaeva <alice.trueorfalse@yandex.ru>
2018-02-15 20:39:58 +0400

    js

--- js/base.js
+++ js/base.js
@@ -14,8 +14,48 @@ hamburger.classList.add("hamburger-active");
 });
 
 
-var menu__accordeon = document.getElementById("menu__accordeon-item");
-console.log (menu__accordeon);
+
+
+
+// var team_name = document.getElementsByClassName("team__accordeon-name");
+// console.log (team_name);
+//  var team_content = document.getElementsByClassName("team__accordeon-content");
+// console.log (team_content);
+
+// for (var i=0;i<team_name.length;i++){
+//   team_name[i].addEventListener('click',function() {
+//     if (!(this.classList.contains('team__active'))){
+//       for(var i=0;i<team_name.length;i++){
+//         team_name[i].classList.remove('team__active');
+//       }
+//       this.classList.add('team__active');
+//     }
+// })
+// }
+
+
+
+var itemV = document.getElementsByClassName('team__accordeon-item');
+
+
+var team_accordeon = function () {
+
+    for (var i = 0; i < itemV.length; i++) {
+        itemV[i].addEventListener('click', function (el) {
+
+            el.currentTarget.classList.toggle(' team__accordeon--active');
+
+            for (let i = 0; i < itemV.length; i++) {
+                if (itemV[i] !== el.currentTarget && itemV[i].className === "team__accordeon-item team__accordeon--active") {
+                    itemV[i].classList.remove('team__accordeon--active');
+                }
+            }
+        });
+    };
+}
+
+
+
 
 
 
