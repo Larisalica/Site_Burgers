b05823a825c85845862fa0df901bcd6c3f7fef08
Larisa Kanaeva <alice.trueorfalse@yandex.ru>
2018-02-17 15:53:18 +0400

    slid

--- js/base.js
+++ js/base.js
@@ -17,51 +17,74 @@ hamburger.classList.add("hamburger-active");
 
 
 
-// var team_name = document.getElementsByClassName("team__accordeon-name");
-// console.log (team_name);
-//  var team_content = document.getElementsByClassName("team__accordeon-content");
-// console.log (team_content);
+var team_name = document.getElementsByClassName("team__accordeon-name");
+console.log (team_name);
+ var team_content = document.getElementsByClassName("team__accordeon-content");
+console.log (team_content);
 
-// for (var i=0;i<team_name.length;i++){
-//   team_name[i].addEventListener('click',function() {
-//     if (!(this.classList.contains('team__active'))){
-//       for(var i=0;i<team_name.length;i++){
-//         team_name[i].classList.remove('team__active');
-//       }
-//       this.classList.add('team__active');
-//     }
-// })
-// }
-
-
-
-var itemV = document.getElementsByClassName('team__accordeon-item');
-
-
-var team_accordeon = function () {
-
-    for (var i = 0; i < itemV.length; i++) {
-        itemV[i].addEventListener('click', function (el) {
-
-            el.currentTarget.classList.toggle(' team__accordeon--active');
-
-            for (let i = 0; i < itemV.length; i++) {
-                if (itemV[i] !== el.currentTarget && itemV[i].className === "team__accordeon-item team__accordeon--active") {
-                    itemV[i].classList.remove('team__accordeon--active');
-                }
-            }
-        });
-    };
+for (var i=0;i<team_name.length;i++){
+  team_name[i].addEventListener('click',function() {
+    if (!(this.classList.contains('team__active'))){
+      for(var i=0;i<team_name.length;i++){
+        team_name[i].classList.remove('team__active');
+      }
+      this.classList.add('team__active');
+    }
+})
 }
 
 
 
 
 
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({  
+        items:1
+});
+});
 
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     items:1, nav:true
   });
 });
-  
+
+
+
+$('#order-form').on('submit', submitForm);
+
+function submitForm (ev) {
+    ev.preventDefault();
+    
+    var form = $(ev.target),
+        data = form.serialize(),
+        url = form.attr('action'),
+        type = form.attr('method');
+
+    ajaxForm(form).done(function(msg) {
+        var mes = msg.mes,
+            status = msg.status;
+        
+        if (status === 'OK') {
+            form.append('<p class="success">' + mes + '</p>');
+        } else{
+            form.append('<p class="error">' + mes + '</p>');
+        }
+    }).fail(function(jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
+
+};
+
+// Универсальная функция для работы с формами
+var ajaxForm = function (form) {
+    var data = form.serialize(),
+        url = form.attr('action');
+    
+    return $.ajax({
+        type: 'POST',
+        url: url,
+        dataType : 'JSON',
+        data: data
+    })
+};
